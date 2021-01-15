123b4726326aba09337566c5c2c83bf7d0abe293
charlestoriola <bugesey.em@gmail.com>
2021-01-15 13:13:30 +0100

    Completed log in feature

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,11 +1,14 @@
 import getpass
 import os.path
+from os import system
 
-# def create_file(filename):
-# 	try:
-# 		file = open(filename, "w")
-# 	except:
-# 		print("Error creating file")
+def next():
+    if not input("Press ENTER to continue"):
+        system("clear")
+        return
+    else:
+        system("clear")
+        next()
 
 
 def whitespace_remove(word):
@@ -15,29 +18,34 @@ def whitespace_remove(word):
 			word = "".join(word.split())
 	return word
 
-def sign_up():
+
+def whitespace_found(username):
 	choice = ''
 
-	username = ""
 	while (choice != 'y' or choice != 'Y'):
-		print("Whitespaces are not allowed")
-		username = input("Enter username: ")
-
-
-		if (' ' in username):
-			newusername = whitespace_remove(username)
-			
-			print("\nYou left some spaces there")
-			choice = input("Would you like to enter your username again?(Y/N) ")
-			print("\n")
-			if choice == 'n':
-				print("Whitespaces removed\n")
-				break
-		else:
-			choice = ''
+		newusername = whitespace_remove(username)
+		
+		print("\nYou left some spaces there")
+		choice = input("Would you like to enter your username again?(Y/N) ")
+		print("\n")
+		if choice == 'n' or choice == 'N':
+			print("Whitespaces removed\n")
+			username = newusername
+			print("\n Your username is ", username)
 			break
+		elif choice == 'y' or choice == 'Y':
+			username = input("Enter username: ")
+			if (' ' in username):
+				username = whitespace_found(username)
+			return username
 
-	password = getpass.getpass()
+
+
+def sign_up(username, password):
+	
+	if (' ' in username):
+		username = whitespace_found(username)
+
 
 	username_file = open('Database/usernames.bin', "a+")
 	password_file = open('Database/password.bin', "a+")
@@ -47,3 +55,33 @@ def sign_up():
 
 	username_file.close()
 	password_file.close()
+
+	return username
+
+
+def log_in(username, password):
+
+	username_file = open('Database/usernames.bin', "r")
+	password_file = open('Database/password.bin', "r")
+
+	userfound = False
+	pwfound = False
+
+	for i in username_file:
+		if i[:-1] == username:
+			userfound = True
+			
+
+	for i in password_file:
+		if i[:-1] == password:
+			pwfound = True
+			
+
+	if userfound and pwfound:
+		password_file.close()
+		username_file.close()
+		return True
+	else:
+		username_file.close()
+		password_file.close()
+		return False
--- main.py
+++ main.py
@@ -1,4 +1,40 @@
 from Functions.functions import *
 
+print("Welcome User")
+next()
 
-sign_up()
+flag = False
+while flag == False:
+
+	menu = int(input("1.Sign Up\n2.Log In\n0.Exit\n"))
+	if menu == 1:
+		print("Whitespaces are not allowed")
+		username = input("Enter username: ")
+		password = getpass.getpass()
+
+		username = sign_up(username, password)
+
+		flag = True
+	elif menu == 2:
+		username = input("Enter username: ")
+		password = getpass.getpass()
+
+		success = log_in(username, password)
+
+		if success:
+			print("Success\n")
+			flag = True
+		else:
+			print("Try Again\n")			
+
+	elif menu == 0:
+		exit()
+		
+	else:
+		print("Invalid Choice\n")
+
+
+system("clear")
+print("Welcome ", username)
+next()
+print("What's good?")
